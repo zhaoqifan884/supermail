85e5b517bec8c0b1d2dfc1abd84eda5cb001f50b
zhaoqifan884 <584123809@qq.com>
2020-03-10 17:09:14 +0800

    购物车导航栏

--- src/views/cart/Cart.vue
+++ src/views/cart/Cart.vue
@@ -1,18 +1,23 @@
 <template>
   <div id="cart">
     <nav-bar class="cart-nav">
-      <div slot="center">购物车</div>
+      <div slot="center">购物车({{cartLength}})</div>
     </nav-bar>
   </div>
 </template>
 
 <script>
   import NavBar from "components/common/navbar/NavBar";
+  //vuex提供方法，可以直接将vuex中getters的方法转成计算属性使用
+  import {mapGetters} from 'vuex'
 
   export default {
     name: "Cart",
     components: {
       NavBar
+    },
+    computed: {
+     ...mapGetters(['cartLength'])
     }
   }
 </script>
